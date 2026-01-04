0850903ba492269e07091c98b537cc8852fd4cd9
Sarthak Singh <yadavsarthak111@gmail.com>
2026-01-04 22:30:27 +0530

    Add files via upload

---
+++ even_odd_function.py
@@ -0,0 +1,12 @@
+# Program to check even or odd using function
+
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+number = int(input("Enter a number: "))
+result = check_even_odd(number)
+
+print("The number is", result)
