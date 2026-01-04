703ffb83a5d9edafc79767a47bd8f0f4d5b7a4c3
Sarthak Singh <yadavsarthak111@gmail.com>
2026-01-04 22:30:07 +0530

    Add files via upload

---
+++ sum_of_digits.py
@@ -0,0 +1,11 @@
+# Program to find sum of digits of a number
+
+num = int(input("Enter a number: "))
+sum_digits = 0
+
+while num > 0:
+    digit = num % 10
+    sum_digits += digit
+    num //= 10
+
+print("Sum of digits:", sum_digits)
