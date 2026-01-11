768af8f1441d329d0a62799b5b67d4cda2295ad0
Sarthak Singh <yadavsarthak111@gmail.com>
2026-01-11 21:51:18 +0530

    Add files via upload

---
+++ traffic_light.py
@@ -0,0 +1,12 @@
+# Traffic Light Simulator
+
+color = input("Enter traffic light color (red / yellow / green): ").lower()
+
+if color == "red":
+    print("🛑 Stop")
+elif color == "yellow":
+    print("⚠ Ready")
+elif color == "green":
+    print("✅ Go")
+else:
+    print("❌ Invalid color")
