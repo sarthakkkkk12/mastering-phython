3db6b4bd6f38abeb98a960abef3b7d7c6cf4c593
Sarthak Singh <yadavsarthak111@gmail.com>
2026-01-11 21:52:33 +0530

    Update atm_withdrawal.py

--- atm_withdrawal.py
+++ atm_withdrawal.py
@@ -4,12 +4,13 @@ balance = float(input("Enter your account balance: "))
 withdraw = float(input("Enter withdrawal amount: "))
 
 if withdraw > balance:
-    print("❌ Insufficient Balance")
+    print(" Insufficient Balance")
 
 elif withdraw % 100 != 0:
-    print("❌ Enter amount in multiples of 100")
+    print(" Enter amount in multiples of 100")
 
 else:
     balance -= withdraw
-    print("✅ Withdrawal Successful")
+    print(" Withdrawal Successful")
+
     print("Remaining Balance:", balance)
