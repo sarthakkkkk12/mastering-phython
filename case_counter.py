ebc35aa44cc2f26aa75c54f0d86f0f1269138212
Sarthak Singh <yadavsarthak111@gmail.com>
2026-01-13 21:50:04 +0530

    Add files via upload

---
+++ case_counter.py
@@ -0,0 +1,15 @@
+# Program to count uppercase and lowercase letters
+
+text = input("Enter a string: ")
+
+upper_count = 0
+lower_count = 0
+
+for char in text:
+    if char.isupper():
+        upper_count += 1
+    elif char.islower():
+        lower_count += 1
+
+print("Uppercase letters:", upper_count)
+print("Lowercase letters:", lower_count)
